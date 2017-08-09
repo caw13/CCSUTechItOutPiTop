f4aa694d72f427f24db3b1d243bdb0a0d15d435a
Chad Williams <chadgit13@gmail.com>
2017-08-09 09:44:28 -0400

    Basic text hangman game

---
+++ python/hangman.py
@@ -0,0 +1,114 @@
+import random
+
+def pick_a_word():
+    word = input("Enter word for opponent to guess:  ")
+    for i in range(0,50):
+        print("")
+    return word
+
+def init_game():
+    global won
+    global guessed_letters
+    global lives_remaining
+    global word
+    guessed_letters = (" ")
+    won = False
+    word = pick_a_word()
+    lives_remaining = 6
+    #print(word)
+
+def get_guess():
+    guess = input("guess a letter or the word: ")
+    print("guess: " + guess)
+    check_guess(guess)
+
+def output_current_known():
+    global guessed_letters, word
+    if len(guessed_letters) > 0:
+        print("guessed letters: "+guessed_letters)
+    output_word = ""
+    for letter in word:
+        if guessed_letters.find(letter) >= 0:
+            output_word += letter + " "
+        else:
+            output_word += "_ "
+    print(output_word)
+
+def draw_man():
+    global lives_remaining
+    print("\n\n\n\n")
+    if lives_remaining < 6:
+        print(" O")
+    if lives_remaining == 4:
+        print(" | ")
+    if lives_remaining == 3:
+        print("/|")
+    if lives_remaining <= 2:
+        print("/|\\")
+    if lives_remaining == 1:
+        print("/")
+    if lives_remaining == 0:
+        print("/ \\")
+        
+
+def check_guess(guess):
+    if len(guess) == 1:
+        check_letter(guess)
+    elif len(guess) > 1:
+        check_word(guess)
+    else:
+        print("Invalid guess, try again")
+    
+
+def check_letter(guess):
+    global word, guessed_letters, lives_remaining
+    if (word.find(guess) > -1):
+        print("contains letter!")
+    else:
+        print("incorrect")
+        lives_remaining -= 1
+    if len(guessed_letters) == 0:
+        guessed_letters = (guess)
+    else:
+        guessed_letters += guess
+    check_if_won()
+
+def check_word(guess):
+    global won, lives_remaining
+    if (word == guess):
+        print("you guessed the word!!!")
+        won = True
+    else:
+        print("incorrect")
+        lives_remaining -= 1
+
+def check_if_won():
+    global word, guessed_letters, won
+    for letter in word:
+        if guessed_letters.find(letter) == -1:
+            return
+    print("You guessed all the letters!")
+    won = True
+
+def you_win():
+    global word
+    print("You won!  You discovered the word was '"+word+"'")
+
+def run_game():
+    global lives_remaining, won, word
+    while True:
+        while lives_remaining > 0 and not won:
+            output_current_known()
+            get_guess()
+            draw_man()
+        if won:
+            print("You won!  You discovered the word was '"+word+"'")
+        else:
+            print("You person was hung, sad state :(")
+            print("The word was '"+word+"'")
+        print("\n\n\n")
+        init_game()
+
+init_game()
+run_game()
+print("THE END")
